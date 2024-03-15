03f0a613beacd044e978ad75a3a6a5c92ed88de6
Michael Taggart <mtaggart@taggart-tech.com>
2024-03-14 22:10:04 -0700

    Add docstrings

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,20 +1,30 @@
 mod getsystem;
 mod persist;
 
+///
+/// The enum that defines the commands we'll interpret 
+/// directly instead of as shell commands 
+/// 
 #[derive(Debug)]
 pub enum C2Command {
     Persist,
     GetSystem,
 }
 
+///
+/// Parses the incoming command text into a known [C2Command]
+/// 
 pub fn parse_command(cmd_str: &str) -> Option<C2Command> {
-    match cmd_str {
+    match cmd_str.trim() {
         "!persist" => Some(C2Command::Persist),
         "!getsystem" => Some(C2Command::GetSystem),
         _ => None,
     }
 }
 
+///
+/// Passes the Command type to its handler, along with any necessary args
+/// 
 pub fn handle(c2_command: C2Command) -> Result<String, String> {
     use C2Command::*;
     match c2_command {
